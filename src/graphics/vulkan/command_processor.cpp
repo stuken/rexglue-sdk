e50ad68cdb6fef1d809e72bf25b0a51a33dc0780
Stuart Kenny <stuartkenny@gmail.com>
2026-08-30 13:23:11 +0100

    fix(gpu): use backface stencil values only when culling only front faces
    Port of xenia 77597d62a: with separate stencil mask/ref support
    unavailable, the backface RB_STENCILREFMASK_BF values were applied
    whenever cull_front was set, even when cull_back was also set (no faces
    culled between the two) - which inverted stencil operations. Only use
    the backface values when culling front faces only.
    
    Branch: sync/xenia-vulkan-zpd

--- src/graphics/vulkan/command_processor.cpp
+++ src/graphics/vulkan/command_processor.cpp
@@ -5936,9 +5936,11 @@ void VulkanCommandProcessor::UpdateDynamicState(const draw_util::ViewportInfo& v
           stencil_ref_mask_back_reg = XE_GPU_REG_RB_STENCILREFMASK_BF;
         } else {
           // Choose the back face values only if drawing only back faces.
-          stencil_ref_mask_front_reg = regs.Get<reg::PA_SU_SC_MODE_CNTL>().cull_front
-                                           ? XE_GPU_REG_RB_STENCILREFMASK_BF
-                                           : XE_GPU_REG_RB_STENCILREFMASK;
+          auto pa_su_sc_mode_cntl = regs.Get<reg::PA_SU_SC_MODE_CNTL>();
+          stencil_ref_mask_front_reg =
+              (pa_su_sc_mode_cntl.cull_front && !pa_su_sc_mode_cntl.cull_back)
+                  ? XE_GPU_REG_RB_STENCILREFMASK_BF
+                  : XE_GPU_REG_RB_STENCILREFMASK;
           stencil_ref_mask_back_reg = stencil_ref_mask_front_reg;
         }
       } else {
